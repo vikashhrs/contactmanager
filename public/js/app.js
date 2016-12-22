fdcaccede6eaf549d90c5fd9a7cf955ff1f08483
Vikash Kumar Sharma <vikashhrs@gmail.com>
2016-12-22 12:12:32 +0530

    Commit1

--- public/js/app.js
+++ public/js/app.js
@@ -6,13 +6,6 @@ var app = angular.module('myApp', ['ngRoute', 'ngCookies']);
 app.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
-            resolve : {
-                "check" : function ($location,$rootScope) {
-                    if(!$rootScope.token){
-                        $location.path("/contacts");
-                    }
-                }
-            },
             templateUrl : "/static/views/login.html",
             controller : "LoginController"
         })
@@ -30,92 +23,42 @@ app.config(function ($routeProvider) {
 
 });
 
-app.run(function ($cookies, $rootScope) {
+app.run(function ($cookies, $rootScope,$location) {
     console.log("Run executed");
     if ($cookies.get('token') && $cookies.get('currentUser')) {
         $rootScope.token = $cookies.get('token');
         console.log($rootScope.token);
         $rootScope.currentUser = $cookies.get('currentUser');
+        $location.path('/contacts')
     }
 });
 
 app.controller('LoginController',['$scope','$location','$http','$cookies','$rootScope',function ($scope,$location,$http,$cookies,$rootScope) {
     //Login
-    $scope.loginError = false;
-    $scope.loginErrorMsg = "vvvhvhvhhchcc";
-    $scope.login = function () {
-        //console.log();
-        if(!$scope.email && !$scope.password){
-            //$scope.testing = "cscscscc";
-            //console.log("email and Password cannot be empty!");
-            $scope.loginError = true;
-            //console.log($scope.loginError);
-            $scope.loginErrorMsg = "Email and Password cannot be empty!";
-
-            //console.log($scope.loginError);
-        }else {
-            if (!$scope.email) {
-                //alert("1")
-                //console.log("Email cannot be empty");
-                $scope.loginErrorMsg = "Email cannor be empty";
-                $scope.loginError = true;
-            } else if (!$scope.password) {
-                //alert("2");
-                //console.log("Password cannot be empty");
-                $scope.loginErrorMsg = "Password cannot be empty";
-                $scope.loginError = true;
-            }else if(!validateEmail($scope.email)){
-                //console.log("Please enter a valid email");
-                $scope.loginErrorMsg = "Please enter a valid email";
-                $scope.loginError = true;
-            } else {
-                //alert("3");
-                $http.put('/users/signin', {
-                    email: $scope.email,
-                    password: $scope.password
-                }).then(function (response) {
-                    console.log(response.data.token);
-                    $cookies.put('token', response.data.token);
-                    $cookies.put('currentUserEmail', $scope.email);
-                    $cookies.put('currentUser',response.data.name);
-                    $rootScope.token = response.data.token;
-                    $rootScope.currentUserEmail = $scope.email;
-                    $rootScope.currentUser = response.data.name;
-                    $scope.loginError = false;
-                    $scope.email = null;
-                    $scope.password = null;
-                    $location.path('/contacts');
-                    //alert(response.data);
-                }, function (err) {
-                    console.log(err);
-                    //alert(err.data);
-                    $scope.loginError = true;
-                    $scope.loginErrorMsg = err.data;
-                });
-            }
-        }
-        function validateEmail(email) {
-            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
-        }
-    }
-
-
-
-
-
-
-    //SignUp
+    $scope.global = {};
     $scope.modalHeader = "Fill in details to signup!"
     $scope.welcome = false;
     $scope.signupform = true;
     $scope.showError = false;
     $scope.error = "";
+
+    $scope.showLoginError = true;
+
+
+    $scope.login = function (value) {
+        $scope.showLoginError = value;
+    }
+
+
+
+    //SignUp
+
     $scope.signUp = function () {
         console.log();
         if(!$scope.name && !$scope.semail && !$scope.spassword && !$scope.matchpassword){
             $scope.showError = true;
             $scope.error = "Please fill in all the fields!";
+            alert($scope.error);
         }else if(!$scope.name){
             $scope.showError = true;
             $scope.error = "Please enter your name!";
@@ -166,6 +109,7 @@ app.controller('LoginController',['$scope','$location','$http','$cookies','$root
             return re.test(email);
         }
     };
+
 
 }]);
 app.controller('ContactsContoller',['$scope','$location','$cookies','$rootScope',function ($scope,$location,$cookies,$rootScope) {
