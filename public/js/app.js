9759ef3cad73083bb76b2b0914b55f88d5ea5e03
Vikash Kumar Sharma <vikashhrs@gmail.com>
2016-12-22 19:55:49 +0530

    Commit12

--- public/js/app.js
+++ public/js/app.js
@@ -42,12 +42,70 @@ app.controller('LoginController',['$scope','$location','$http','$cookies','$root
     $scope.showError = false;
     $scope.error = "";
 
-    $scope.showLoginError = true;
+    $scope.loginError = false;
+    $scope.loginErrorMsg = "vvvhvhvhhchcc";
+    $scope.login = function () {
+        //console.log();
+        if(!$scope.email && !$scope.password){
+            //$scope.testing = "cscscscc";
+            //console.log("email and Password cannot be empty!");
 
-
-    $scope.login = function (value) {
-        $scope.showLoginError = value;
+            $scope.loginError = true;
+            //console.log($scope.loginError);
+            $scope.loginErrorMsg = "Email and Password cannot be empty!";
+            alert($scope.loginErrorMsg);
+            //console.log($scope.loginError);
+        }else {
+            if (!$scope.email) {
+                //alert("1")
+                //console.log("Email cannot be empty");
+                $scope.loginErrorMsg = "Email cannor be empty";
+                $scope.loginError = true;
+                alert($scope.loginErrorMsg);
+            } else if (!$scope.password) {
+                //alert("2");
+                //console.log("Password cannot be empty");
+                $scope.loginErrorMsg = "Password cannot be empty";
+                $scope.loginError = true;
+                alert($scope.loginErrorMsg);
+            }else if(!validateEmail($scope.email)){
+                //console.log("Please enter a valid email");
+                $scope.loginErrorMsg = "Please enter a valid email";
+                $scope.loginError = true;
+                alert($scope.loginErrorMsg);
+            } else {
+                //alert("3");
+                $http.put('/users/signin', {
+                    email: $scope.email,
+                    password: $scope.password
+                }).then(function (response) {
+                    console.log(response.data.token);
+                    $cookies.put('token', response.data.token);
+                    $cookies.put('currentUserEmail', $scope.email);
+                    $cookies.put('currentUser',response.data.name);
+                    $rootScope.token = response.data.token;
+                    $rootScope.currentUserEmail = $scope.email;
+                    $rootScope.currentUser = response.data.name;
+                    $scope.loginError = false;
+                    $scope.email = null;
+                    $scope.password = null;
+                    $location.path('/contacts');
+                    //alert(response.data);
+                }, function (err) {
+                    console.log(err);
+                    //alert(err.data);
+                    $scope.loginError = true;
+                    $scope.loginErrorMsg = err.data;
+                    alert($scope.loginErrorMsg);
+                });
+            }
+        }
+        function validateEmail(email) {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email);
+        }
     }
+
 
 
 
@@ -85,7 +143,7 @@ app.controller('LoginController',['$scope','$location','$http','$cookies','$root
                     console.log(response);
                     if(response.status === "present"){
                         $scope.showError = true;
-                        $scope.error = "Email ID already used";
+                        $scope.error = "Email ID used";
                         $scope.semail = null;
                     }else{
                         var user = {
@@ -99,11 +157,11 @@ app.controller('LoginController',['$scope','$location','$http','$cookies','$root
                             $scope.signupform = false;
                             $scope.modalHeader = "Succesfully signed up."
                             console.log(response);
-                        })
+                        });
                     }
                 });
             }
-        }
+        };
         function validateEmail(email) {
             var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(email);
@@ -112,7 +170,21 @@ app.controller('LoginController',['$scope','$location','$http','$cookies','$root
 
 
 }]);
-app.controller('ContactsContoller',['$scope','$location','$cookies','$rootScope',function ($scope,$location,$cookies,$rootScope) {
+app.controller('ContactsContoller',['$scope','$location','$cookies','$rootScope','$http',function ($scope,$location,$cookies,$rootScope,$http) {
+
+    $scope.contacts = null;
+    loadContacts();
+    function loadContacts(){
+        $http.get('/contacts/receive').success(function (response) {
+            console.log(response);
+            $scope.contacts = response.reverse();
+            //$timeout(loadMeows(),5000);
+        });
+    }
+
+
+    $scope.addContactHeader = "Fill in all details to add contact";
+    $scope.succesMsg = "";
     $scope.logout = function () {
         console.log("Log out");
         $cookies.remove('token');
@@ -121,6 +193,62 @@ app.controller('ContactsContoller',['$scope','$location','$cookies','$rootScope'
         $rootScope.token = null;
         $rootScope.currentUser = null;
         $location.path("/");
-    }
+    };
+    $scope.addError = false;
+    $scope.addErrorMsg = "";
+    $scope.addContact = function () {
+        if(!$scope.name && !$scope.email && !$scope.number){
+            $scope.addError = true;
+            $scope.addErrorMsg = "Please Fill in all details to add contact";
+        }else if(!$scope.name){
+            $scope.addError = true;
+            $scope.addErrorMsg = "Please Fill in persons name";
+        }else if(!$scope.email){
+            $scope.addError = true;
+            $scope.addErrorMsg = "Please Fill in person's email";
+        }else if(!validateEmail($scope.email)){
+            $scope.addError = true;
+            $scope.addErrorMsg = "Please Fill in valid email";
+            $scope.email = "";
+        }else if(!$scope.number){
+            $scope.addError = true;
+            $scope.addErrorMsg = "Please Fill in contact number";
+        }else if(!validateNumber($scope.number)){
+            $scope.addError = true;
+            $scope.addErrorMsg = "Please Fill in valid number";
+        }else{
+            $http.post('/check/users',{email : $scope.email}).success(function (response) {
+                console.log(response);
+                if(response.status === "present"){
+                    $scope.addError = true;
+                    $scope.addErrorMsg = "Email ID used";
+                    $scope.email = null;
+                }else{
+                    $http.post('/contacts/add',{name : $scope.name,number : $scope.number,email : $scope.email},{headers: {'authorization': $rootScope.token}}).success(function (response) {
+                        console.log("Sending post request");
+                        console.log(response);
+                        $scope.addContactHeader = "Succesfull!!!"
+                        $scope.succesMsg = "Contact Added";
+                        loadContacts();
+                    });
+                }
+            });
+        }
+
+        function validateEmail(email) {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email);
+        }
+        function validateNumber(number){
+            if(number.length == 10){
+                return true;
+            }
+            return false;
+        }
+        function getNumber(number){
+            return parseInt(number);
+        }
+    };
+
 }]);
 
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
  */
 var express = require('express');
 var mongoose = require('mongoose');
-
+var PORT = process.env.PORT || 3000;
 var morgan = require('morgan');
 var jwt = require('jwt-simple');
 var JWT_SECRET = "24446666688888889";
@@ -14,39 +14,39 @@ var Contact = require('./models/contacts');
 
 
 var app = express();
-mongoose.connect("mongodb://localhost:27017/contactmanager");
-
+//mongoose.connect("mongodb://localhost:27017/contactmanager");
+mongoose.connect("mongodb://vikashhrs:12345@ds015902.mlab.com:15902/contactmanager");
+//mongodb://vikashhrs:12345@ds015902.mlab.com:15902/contactmanager
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/static', express.static(__dirname + '/public'));
 
 
-
-app.get('/',function (req,res) {
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.put('/users/signin',function (req,res) {
-    if(!req.body){
+app.put('/users/signin', function (req, res) {
+    if (!req.body) {
         console.log("parameters received");
     }
     console.log(req.body);
     console.log(req.body.email);
-    User.findOne({ email : req.body.email},function (err,user) {
+    User.findOne({email: req.body.email}, function (err, user) {
         console.log(err);
         console.log(user);
-        if(err)
+        if (err)
             throw err;
-        if(user) {
+        if (user) {
             if (bcrypt.compareSync(req.body.password, user.password)) {
-                var token = jwt.encode(user,JWT_SECRET);
-                res.status(200).send({token : token,name : user.name});
+                var token = jwt.encode(user, JWT_SECRET);
+                res.status(200).send({token: token, name: user.name});
             } else {
                 res.status(400).send("Incorrect password");
             }
         }
-        if(!user){
+        if (!user) {
             res.status(400).send("No user found");
         }
     });
@@ -55,17 +55,16 @@ app.put('/users/signin',function (req,res) {
 });
 
 
-
-app.post('/users',function (req,res) {
+app.post('/users', function (req, res) {
     console.log(req.body.user);
-    req.body.user.password = bcrypt.hashSync(req.body.user.password,bcrypt.genSaltSync(9));
+    req.body.user.password = bcrypt.hashSync(req.body.user.password, bcrypt.genSaltSync(9));
     //console.log(req.body);
     var user = new User(req.body.user);
     /*var newUser = req.body.user
      newUser.password = bcrypt.hashSync(req.body.user.password,bcrypt.genSaltSync(9));
      var user = new User(newUser);*/
     user.save(function (err) {
-        if(err){
+        if (err) {
             throw err;
         }
 
@@ -74,7 +73,7 @@ app.post('/users',function (req,res) {
     res.send('Saved');
 });
 
-app.post('/check/users',function (req,res) {
+app.post('/check/users', function (req, res) {
     console.log(req.body);
     User.findOne({email : req.body.email},function (err,user) {
         if(err)
@@ -83,13 +82,51 @@ app.post('/check/users',function (req,res) {
             res.send({status : "present"});
         }
         if(!user){
-            res.send({status : "notpresent"});
+            //res.send({status : "notpresent"});
+            Contact.findOne({email : req.body.email},function (err,user) {
+                if(err)
+                    throw err;
+                if(user){
+                    res.send({status : "present"});
+                }
+                if(!user){
+                    res.send({status : "notpresent"});
+                }
+            })
         }
-    });
-    //res.send({status : "Happening"});
+    })
 });
 
-app.listen(3000,function () {
+app.post('/contacts/add', function (req, res) {
+    console.log(req.body);
+    console.log(req.headers.authorization);
+    var user = jwt.decode(req.headers.authorization, JWT_SECRET);
+    console.log(user);
+    var contact = new Contact({
+        name: req.body.name,
+        email: req.body.email,
+        contactnumber: req.body.number,
+        addedBy: {
+            id: user._id,
+            name: user.name
+        }
+    });
+    console.log(contact);
+    contact.save(function (err) {
+        if(err)
+            throw err;
+    })
+    res.send({message: "Data Added"})
+});
+app.get("/contacts/receive",function (req,res) {
+    Contact.find(function (err,contacts) {
+        if(err)
+            throw err;
+        console.log(contacts)
+        res.send(contacts);
+    })
+})
+app.listen(PORT, function () {
     console.log("Server running on port 3000");
 })
 
